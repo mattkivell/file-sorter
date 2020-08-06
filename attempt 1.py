cd3e1378ba316daad141f36c83c56e076c26f0c9
mattkivell <kivellm@stu.otc.school.nz>
2020-08-06 15:15:10 +1200

    new update 6 attempting ui want to cry that rhymes

--- attempt 1.py
+++ attempt 1.py
@@ -1,11 +1,22 @@
-from tkinter import *
+import tkinter as tk
 import os
-master = Tk()
+class App(tk.Frame):
+    def _init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.pack()
+        self.createwidget()
+
+
+master = tk.Tk()
+myfortnite = App()
+
+myfortnite.master.title("big chungus")
+myfortnite.master.maxsize(1920,1080)
 
 def readprint():
     readprint = str(input("what file do you want to read 1 , 2 , 3 ? "))
     file = open(readprint,"r")
-    if file.mode == 'r' :
+    if file.mode == 'r':
         contents =file.read()
         print(contents)
 
@@ -14,7 +25,7 @@ def overwrite():
     write = str(input("what do you want to write? "))
     file = open(overwrite,"w")
     file.write("{}".format(write))
-    print("the file now says".format(write))
+    print("the file now says" " {}".format(write))
 
 def createfolder():
     os.chdir('G:\My Drive')
@@ -25,3 +36,20 @@ def createfolder():
 def createfile():
     createfile = str(input("what do you want to name the file? "))
     file = open(createfile,"w+")
+
+def createwidget(self):
+    self.chungus = tk.Button(self)
+    self.chungus["text"] = "???\n(clickme)"
+    self.chungus["command"] = self.chunngus
+    self.chungus.pack(side="top")
+
+    self.quit = tk.Button(self, text="QUIT", fg="red")
+
+    self.quit.pack(side="bottom")
+
+def chunngus(self):
+    print("BIG CHUNGUS")
+root = tk.Tk()
+myfortnite = App(master=root)
+myfortnite.mainloop()
+#mank
