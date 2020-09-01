320177fbf053c8dadb151ceb14ad69d58f8ee29d
mattkivell <kivellm@stu.otc.school.nz>
2020-09-01 13:28:59 +1200

    new bad boy

--- attempt 2.py
+++ attempt 2.py
@@ -1,50 +1,51 @@
 import tkinter as tk
 import os
+window=Tk()
 class Application(tk.Frame):
-    def __init__(penis, master=None):
-        tk.Frame.__init__(penis, master)
-        penis.pack()
-        penis.buttoneditfile()
-        penis.buttonreadfile()
-        penis.buttoncreatefile()
-    def buttoneditfile(penis):
-        penis.BEF = tk.Button(penis)
-        penis.BEF["text"] = "do you want to edit a file\n(click me)"
-        penis.BEF["command"] = penis.overwrite
-        penis.BEF.pack(side="top")
+    def __init__(, master=None):
+        tk.Frame.__init__(, master)
+        .pack()
+        .buttoneditfile()
+        .buttonreadfile()
+        .buttoncreatefile()
+    def buttoneditfile():
+        .BEF = tk.Button()
+        .BEF["text"] = "do you want to edit a file\n(click me)"
+        .BEF["command"] = .overwrite
+        .BEF.pack(side="top")
 
-    def buttonreadfile(penis):
-        penis.BRF = tk.Button(penis)
-        penis.BRF["text"] = "do you want to read the file\n(click me)"
-        penis.BRF["command"] = penis.readprint
-        penis.BRF.pack(side="bottom")
+    def buttonreadfile():
+        .BRF = tk.Button()
+        .BRF["text"] = "do you want to read the file\n(click me)"
+        .BRF["command"] = .readprint
+        .BRF.pack(side="bottom")
 
-    def buttoncreatefile(penis):
-        penis.BCF = tk.Button(penis)
-        penis.BCF["text"] = "do you want to create a file\n(click me)"
-        penis.BCF["command"] = penis.createfile
-        penis.BCF.pack(side="bottom")
-    def readprint(penis):
+    def buttoncreatefile():
+        .BCF = tk.Button()
+        .BCF["text"] = "do you want to create a file\n(click me)"
+        .BCF["command"] = .createfile
+        .BCF.pack(side="bottom")
+    def readprint():
         readprint = str(input("what file do you want to read 1 , 2 , 3 ? "))
         file = open(readprint,"r")
         if file.mode == 'r':
             contents =file.read()
             print(contents)
 
-    def overwrite(penis):
+    def overwrite():
         overwrite = str(input("what file do you want to edit 1 , 2 , 3 ? "))
         write = str(input("what do you want to write? "))
         file = open(overwrite,"w")
         file.write("{}".format(write))
         print("the file now says" " {}".format(write))
 
-    def createfolder(penis):
+    def createfolder():
         os.chdir('G:\My Drive')
         foldy = str(input(" folder name: "))
         os.mkdir(foldy)
         print("Folder created called {}".format(foldy))
 
-    def createfile(penis):
+    def createfile():
         createfile = str(input("what do you want to name the file? "))
         file = open(createfile,"w+")
 
