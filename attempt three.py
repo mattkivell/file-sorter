b2e5c5ac52d7f5b35030819cb5461fc302593f1d
mattkivell <kivellm@stu.otc.school.nz>
2020-09-02 10:53:34 +1200

    fixed UI agian for the 2nd time

--- attempt three.py
+++ attempt three.py
@@ -2,17 +2,27 @@ from tkinter import *
 import os
 class Mywindow:
     def __init__(self, win):
-        self.label1=Label(win, text='Would you like to edit a text file')
-        self.label2=Label(win, text='Would you like to make a new  text file')
-        self.label3=Label(win, text='Would you like to read a text file')
-        self.text1=Entry(bd=2)
+        self.label1=Label(win, text='What text file do you want to read')
         self.text2=Entry(bd=2)
-        self.text3=Entry(bd=2)
-        self.button1 = Button(win, text,'Search')
-        self.text1.bind(win, command=self.editfile)
-        self.text2.bind(win, command=self.newfile)
-        self.text3.bind(win, command=self.readfile)
+        self.text1=Entry(bd=2)
+        self.button1 = Button(win, text='Search')
+        self.button1.bind('button 1', self.readprint)
+        self.button1.place(x=140, y=150)
+        self.text1.place(x=100, y=100)
+        self.label1.place(x=80, y=50)
+        self.text2.place(x=100, y=220)
+    def readprint(self):
+        self.text1 = str(input())
+        file = open(self.text2,"r")
+        if file.mode == 'r':
+            contents =file.read()
+            self.text2.insert(self,contents)
 
 
+window=Tk()
+mywin=Mywindow(window)
+window.title('file editor')
+window.geometry("300x300+10+10")
+window.mainloop()
 
 
